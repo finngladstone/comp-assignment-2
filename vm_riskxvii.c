4f2e7357ce30b6a67eb2147705e0e3f04c71ca60
Finn Gladstone <Finngladstone9@gmail.com>
2023-03-28 12:57:27 +1100

    E

--- vm_riskxvii.c
+++ vm_riskxvii.c
@@ -5,61 +5,29 @@
 
 /* Error blocks */
 
-void instruction_invalid(int i) {
+void instruction_invalid(int i, uint32_t * registers, int * program_count) {
     printf("Instruction not implemented - %x\n", i);
-}
-
-void func3_fail(int func3) {
-    printf("Invalid func3 - %i\n", func3);
+    dump_register_banks(registers, *program_count);
     exit(1);
 }
 
-void func7_fail() {
-    printf("Func7 - undefined behaviour.\n");
-    exit(1);
-}
+void parse_files(char * filename, int * instruction_buffer, unsigned char * byte_buffer) {
+    FILE * binaryfile = fopen(filename, "rb");
 
-void opcode_fail() {
-    printf("Opcode - undefined behaviour.\n");
-    exit(1);
-}
-
-void parse_file(char * filename, int * buffer, int size) {
-    FILE * binaryfile;
-
-    binaryfile = fopen(filename, "rb");
     if (binaryfile == NULL) {
         printf("Failed to read file: %s\n", filename);
         exit(1);
     }
 
-    if (fread(buffer, sizeof(buffer[0]), size, binaryfile)) {}
+    if (fread(instruction_buffer, 4, 256, binaryfile)) {}
+    rewind(binaryfile);
+    if (fread(byte_buffer, 1, 2048, binaryfile)) {}
 
     fclose(binaryfile);
-
-    return;
-}
-
-void parse_file_single_byte(char * filename, unsigned char * buffer, int size) {
-    FILE * binaryfile;
-
-    binaryfile = fopen(filename, "rb");
-    if (binaryfile == NULL) {
-        printf("Failed to read file: %s\n", filename);
-        exit(1);
-    }
-
-    if (fread(buffer, sizeof(buffer[0]), size, binaryfile)) {}
-
-    fclose(binaryfile);
-
-    return;
 }
 
 
 /* Function pointer arrays */
-
-// https://stackoverflow.com/questions/252748/how-can-i-use-an-array-of-function-pointers
 
 void (*TYPE_R_Pointer[8])(ARGS2) = {add, sll, slt, sltu, xor, srl, or, and};
 
@@ -73,21 +41,15 @@ void (*TYPE_SB_Pointer[8])(ARGS2) = {beq, bne, NULL, NULL, blt, bge, bltu, bgeu}
 
 /* FUNCTION ROUTER */
  
-void parse_binary(ARGS) { 
-    // printf("%i\n", i);
+void parse_binary(ARGS) {
     struct data codes = { 0 };
-    
-    int opcode_valid = update_data_struct(&codes, i);
-    
-    if (opcode_valid == 0) {
+
+    if (update_data_struct(&codes, i) == 0) {
         printf("Instruction Not Implemented: 0x%x\n", i);
         
         dump_register_banks(registers, *program_count);
         exit(1);
     }
-
-    // printf("Opcode = %x, func3 = %i, func7 = %i, RD = %i, rs1 = %i, rs2 = %i, imm = %i\n", 
-    //     codes.opcode, codes.func3, codes.func7, codes.rd, codes.rs1, codes.rs2, codes.imm);
 
     switch(codes.opcode) {
 
@@ -99,16 +61,16 @@ void parse_binary(ARGS) {
                 else if (codes.func3 == 5) 
                     sra(ARGS3);
                 else 
-                    func3_fail(codes.opcode);
+                    instruction_invalid(i, registers, program_count);
             } 
             
             else if (codes.func7 == 0) {
                 if (within_range(0, 7, codes.func3))
                     (*TYPE_R_Pointer[codes.func3])(ARGS3);
                 else 
-                    func3_fail(codes.opcode);
+                    instruction_invalid(i, registers, program_count);
             } else 
-                func7_fail();
+                instruction_invalid(i, registers, program_count);
             
             break;
         }
@@ -118,7 +80,7 @@ void parse_binary(ARGS) {
             if (within_range(0, 7, codes.func3) && codes.func3 != 1 && codes.func3 != 5) {
                 (*TYPE_I_Pointer[codes.func3])(ARGS3);
             } else {
-                func3_fail(codes.opcode);
+                instruction_invalid(i, registers, program_count);
             }
 
             break;
@@ -129,7 +91,7 @@ void parse_binary(ARGS) {
             if (within_range(0, 5, codes.func3) && codes.func3 != 3) {
                 (*TYPE_I_Pointer2[codes.func3])(ARGS3);
             } else {
-                func3_fail(codes.opcode);
+                instruction_invalid(i, registers, program_count);
             }
 
             break;
@@ -140,7 +102,7 @@ void parse_binary(ARGS) {
             if (codes.func3 == 0x0)
                 jalr(ARGS3);
             else
-                func3_fail(codes.opcode);
+                instruction_invalid(i, registers, program_count);
             
             break;
         }
@@ -150,7 +112,7 @@ void parse_binary(ARGS) {
             if (within_range(0, 2, codes.func3)) {
                 (*TYPE_S_Pointer[codes.func3])(ARGS3);
             } else {
-                func3_fail(codes.opcode);
+                instruction_invalid(i, registers, program_count);
             }
 
             break;
@@ -162,7 +124,7 @@ void parse_binary(ARGS) {
             if (within_range(0, 1, codes.func3) || within_range(4, 7, codes.func3)) {
                 (*TYPE_SB_Pointer[codes.func3])(ARGS3);
             } else {
-                func3_fail(codes.opcode);
+                instruction_invalid(i, registers, program_count);
             }
 
             break;
@@ -182,7 +144,7 @@ void parse_binary(ARGS) {
 
         default:
         {
-            printf("This should not occur\n");
+            instruction_invalid(i, registers, program_count);
             exit(1);
         }
     }
@@ -199,8 +161,7 @@ int main(int argc, char * argv[]) {
     unsigned char memory_byte_arr[MEMORY_BYTE_ARR_SIZE] = { 0 }; 
  
     // Update arrs (1 4B array for instructions, 1 1B array for data)
-    parse_file(argv[1], instruction_arr, INSTRUCTION_SIZE);
-    parse_file_single_byte(argv[1], memory_byte_arr, MEMORY_BYTE_ARR_SIZE);
+    parse_files(argv[1], instruction_arr, memory_byte_arr);
 
     while (1) {
         parse_binary(instruction_arr[program_counter/4], registers, &program_counter, memory_byte_arr);
@@ -208,9 +169,3 @@ int main(int argc, char * argv[]) {
 
     return 0;
 }
-
-
-
-    
-
-        
